9b30d5d9d0fd273272d82138c48150963a7bcc9e
Ben Brady <86134744+benjamin-brady@users.noreply.github.com>
2022-06-03 00:38:35 +1200

    Fix bug

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@
 	let regex = /(([^\u0000-\u007F]|\w)+)((\W)*)/ig
 	const stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 	let stopwordsDict ={}
-	stopwords.filter(w => w.length < 7).forEach(w => stopwordsDict[w]=1)
+	stopwords.filter(w => w.length < 8).forEach(w => stopwordsDict[w]=1)
 	
 	let titleTokens = []
 	let contentTokens = []
@@ -87,7 +87,6 @@
 		}
 		return tokens
 	}
-	//renderTokens()
 	
 	function selectWord(event) {
 		console.log(event)
@@ -97,7 +96,7 @@
 	
 	function handleSubmit() {
 		selectedWord=guess.toLowerCase()
-		if(selectedWord = 'togglecheats') {
+		if(selectedWord == 'togglecheats') {
 			solved = !solved
 		}
 		let regex = new RegExp(`\\b${selectedWord}\\b`,'gi')
