f67c07a592d02bb485b30cfeea5d8fa75493984b
Ben Brady <86134744+benjamin-brady@users.noreply.github.com>
2022-06-07 11:03:47 +1200

    Make word length hints less conspicuous

--- src/components/Span.svelte
+++ src/components/Span.svelte
@@ -4,9 +4,11 @@
     export let highlight = false;
     export let redacted = false;
 </script>
-    
+{#if value.length > 2}
 <span id={id} class="{(highlight ? 'highlight' : '') + (redacted ? ' redacted' : '')}">{#if redacted}<span class="char_count">{value.length}</span>{/if}{ redacted ? '█'.repeat(value.length - `${value.length}`.length) : value }</span>
-    
+{:else}
+<span id={id} class="{(highlight ? 'highlight' : '') + (redacted ? ' redacted' : '')}">{ redacted ? '█'.repeat(value.length) : value}</span>
+{/if}
 <style>
     .highlight{
         background-color: #00becc;
@@ -16,7 +18,7 @@
 		background-color: #989898;
 	}
     .char_count {
-        color:#333;
-        font-size: 0.8em;
+        color:#555;
+        font-size: 0.75em;
     }
 </style>
